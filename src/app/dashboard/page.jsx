bd1d78e8435dd1b19b8772b8fe3004bd653915ff
sinkoosha <sinkoosha@gmail.com>
2023-09-09 20:27:22 +0330

    pushNavBarRight

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -21,10 +21,6 @@ const Dashboard = () => {
 
   return (
     <div className="flex h-screen">
-      {/* Content Box */}
-      <div className="flex-1 bg-gray-100 p-4">Content goes here</div>
-
-      {/* Right Navbar */}
       <div className="flex-5 bg-gray-800 p-4">
         {/* Information Box */}
         <div className="bg-white p-6 rounded-lg shadow-lg mb-6">
@@ -33,6 +29,10 @@ const Dashboard = () => {
           <ul className="space-y-2"></ul>
         </div>
       </div>
+      {/* Content Box */}
+      <div className="flex-1 bg-gray-100 p-4">Content goes here</div>
+
+      {/* Right Navbar */}
     </div>
   );
 };
