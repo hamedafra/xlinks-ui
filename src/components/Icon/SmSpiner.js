1a2fd8c8543137ac00d2e4b1b7f59dcf3096eafe
root <hamedafra@gmail.com>
2023-09-17 11:52:22 +0000

    full auth

--- src/components/Icon/SmSpiner.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function SmSpiner() {
-  return (
-    <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-red-600"></div>
-  );
-}
-
-export default SmSpiner;
