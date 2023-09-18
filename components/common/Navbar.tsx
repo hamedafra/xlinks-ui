6e7cde386e9cdd35f82d309ad766af0c9c81c2d4
root <hamedafra@gmail.com>
2023-09-18 15:06:52 +0000

    add mylinks

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -94,7 +94,7 @@ export default function Navbar() {
 							<div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
 								<div className='flex flex-shrink-0 items-center'>
 									<NavLink href='/' isBanner>
-										Full Auth
+										MyLinks
 									</NavLink>
 								</div>
 								<div className='hidden sm:ml-6 sm:block'>
